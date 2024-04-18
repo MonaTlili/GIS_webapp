272a971c13aebcabf3729c870124f0cd1e1b62ff
Kronkan <h22thsta@du.se>
2024-04-18 16:16:02 +0200

    Co-authored-by: MonaTlili <MonaTlili@users.noreply.github.com> Co-authored-by: Filip Lindgren <Filiplindgren@users.noreply.github.com> Co-authored-by: Nicklas Kjellbom <Kaffesumpet@users.noreply.github.com>

--- webmapp201/main.py
+++ webmapp201/main.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+import pandas as pd
+from sklearn.cluster import KMeans
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+
+    df= pd.read_csv("webmapp201/static/school_locations.csv")
+    data = df[["xcoord", "ycoord"]]
+    k = 5
+
+    kmeans = KMeans(n_init=20, n_clusters=k, random_state=0)
+    df["cluster"] = kmeans.fit_predict(data)
+
+    points = df.to_dict(orient="records")
+
+    cluster_colors = ["red", "pink", "blue", "orange", "purple", "yellow"]
+    icons = {str(i): url_for('static', filename=f'cluster{cluster_colors[i]}.svg') for i in range(k)}
+
+    return render_template("index.html", points=points, icons=icons)
     
 if __name__ == "__main__":
     app.run(debug=True)
+
