f6dfc5b38627df2dd3188cd6b6284688b243dc85
Kronkan <h22thsta@du.se>
2024-04-23 11:53:57 +0200

    Co-authored-by: MonaTlili <MonaTlili@users.noreply.github.com> Co-authored-by: Nicklas Kjellbom <Kaffesumpet@users.noreply.github.com>

--- webmapp201/main.py
+++ webmapp201/main.py
@@ -13,13 +13,16 @@ def home():
 
     kmeans = KMeans(n_init=20, n_clusters=k, random_state=0)
     df["cluster"] = kmeans.fit_predict(data)
+    centroids = kmeans.cluster_centers_
+    print(centroids) 
 
     points = df.to_dict(orient="records")
+    cluster_centroids = [{"xcoord": center[0], "ycoord": center[1], "type": "centroid"} for center in centroids]
 
     cluster_colors = ["red", "pink", "blue", "orange", "purple", "yellow"]
     icons = {str(i): url_for('static', filename=f'/src/icons/cluster{cluster_colors[i]}.svg') for i in range(k)}
 
-    return render_template("index.html", points=points, icons=icons)
+    return render_template("index.html", points=points, icons=icons, centroids=cluster_centroids)
     
 if __name__ == "__main__":
     app.run(debug=True)
