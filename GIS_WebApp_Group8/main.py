6a84ba3c1cde88a0c7aa8402800a8951994c9b5c
Kronkan <h22thsta@du.se>
2024-05-02 12:47:14 +0200

    Update main.py

--- GIS_WebApp_Group8/main.py
+++ GIS_WebApp_Group8/main.py
@@ -6,21 +6,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-
+    # READING DATA & PERFORMING KMEANS
     df= pd.read_csv("GIS_WebApp_Group8/static/school_locations.csv")
-    data = df[["xcoord", "ycoord"]]
+    coords = df[["xcoord", "ycoord"]]
     k = 6
 
     kmeans = KMeans(n_init=20, n_clusters=k, random_state=0)
-    df["cluster"] = kmeans.fit_predict(data)
+    df["cluster"] = kmeans.fit_predict(coords)
     centroids = kmeans.cluster_centers_
 
     points = df.to_dict(orient="records")
-    # points = df.to_dict(orient="records")
     cluster_centroids = [{"xcoord": center[0], "ycoord": center[1], "clusterNum": i} for i, center in enumerate(centroids)]
 
     cluster_colors = ["red", "pink", "blue", "orange", "purple", "yellow"]
     icons = {str(i): url_for('static', filename=f'/src/icons/cluster{cluster_colors[i]}.svg') for i in range(k)}
+    # END READING DATA & PERFORMING KMEANS
 
     return render_template("index.html", points=points, icons=icons, centroids=cluster_centroids)
     
